b0587dcea63f169099f035b7a6ba96cdfd0dcd1b
Tommaso <tommaso.macco@gmail.com>
2021-10-26 13:10:48 +0200

    Prima struttura e link e chiediamo l'email all'utente

---
+++ script.js
@@ -0,0 +1,9 @@
+// GIOCO DEI DADI, CHIEDERE ALL UTENTE LA SUA EMAIL E VERIFICARE SE ESSA SIA ALL'INTERNO DELLA LISTA CONSENTITA
+
+
+// Iniziamo col chiedere all'utente la tua email e assegnamo il dato ad una variabile
+
+let emailUser = prompt("inserisci la tua email")
+console.log(emailUser);
+
+Creiamo una lista di email consentite grazie ad un array
