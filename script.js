4b95645330f65f6094d6ab8adcdd6270edc00db4
Tommaso <tommaso.macco@gmail.com>
2021-10-26 13:49:32 +0200

    email check finito

--- script.js
+++ script.js
@@ -6,4 +6,20 @@
 let emailUser = prompt("inserisci la tua email")
 console.log(emailUser);
 
-Creiamo una lista di email consentite grazie ad un array
+// Creiamo una lista di email consentite grazie ad un array
+
+const emailApproved= ["giorgio@gmail.com", "mattia@gmail.com", "giorgia@gmail.com", "tommaso@gmail.com"];
+console.log(emailApproved);
+
+// Adesso confrontiamo le email e decidiamo se l'utente può entrare nel sito oppure no
+
+const emailCheck = document.getElementById("email");
+
+if (emailUser=emailApproved) { 
+    emailCheck.innerHTML = "Complimenti rilsuti regisrato/a al sito, puoi accedere."
+}
+else {
+    emailCheck.innerHTML = "Sfortunatamente non risulti registrato/a al sito, registrati per accedere."
+}
+
+console.log(emailCheck);
